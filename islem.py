6211b0f594b97d906f870621645a5dc4c5ba6e6f
keskinhub <48883458+keskinhub@users.noreply.github.com>
2019-05-23 02:09:15 +0300

    ilk yükleme

---
+++ islem.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import dht11
+import time
+import datetime
+import smtplib
+ 
+
+mail = smtplib.SMTP("smtp.gmail.com",587)
+mail.ehlo() 
+mail.starttls()
+mail.login("YourGmailEMail","EmailPassword")
+
+
+# initialize GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+# read data using pin 14
+instance = dht11.DHT11(pin=14)
+
+while True:
+    result = instance.read()
+    if result.is_valid():
+        mail.sendmail("SenderEmailAdress","reciverEmailAdress","Subject: Gunluk Hava Durumu \nMerhaba ,\n\r Bugun Hava Sicakligi= "+repr(result.temperature) +"C derece, Nem = " + repr(result.humidity))        
+    
+    time.sleep(5)
